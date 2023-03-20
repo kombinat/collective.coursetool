342d50a764d12a91fff6c9bdbc8cd6f5d71f94fe
Peter Mathis <peter.mathis@kombinat.at>
2023-03-20 07:27:14 +0100

    fix payment success of exam

--- src/collective/coursetool/shop.py
+++ src/collective/coursetool/shop.py
@@ -20,18 +20,19 @@ def payment_success(data):
     if not uids:
         # should not happen
         return
-    course = api.content.get(UID=uids[0])
+    item = api.content.get(UID=uids[0])
     member = api.content.get(UID=order.attrs.get("creator"))
-    api.relation.create(source=course, target=member, relationship="members")
+    api.relation.create(source=item, target=member, relationship="members")
 
-    for exam in course.get_exams():
-        members = exam.members
-        if len([m for m in members if m["member"] == member.UID()]):
-            # member is already in members
-            continue
-        members.append({"member": member.UID(), "success": ()})
-        exam.members = members
-        exam.reindexObject()
+    if item.portal_type == "coursetool.course":
+        for exam in item.get_exams():
+            members = exam.members
+            if len([m for m in members if m["member"] == member.UID()]):
+                # member is already in members
+                continue
+            members.append({"member": member.UID(), "success": ()})
+            exam.members = members
+            exam.reindexObject()
 
 
 CHECKOUT_FIELD_MAP = {
